7f13d1d248ffed20cd6407b09820b89365b6c9f6
NiranPrem <163099269+NiranPrem@users.noreply.github.com>
2026-08-28 11:58:17 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,7 @@
 output "resource_group_name" {
-  description = "Name of the resource group"
-  value       = azurerm_resource_group.demo.name
+  value = azurerm_resource_group.demo.name
 }
 
 output "resource_group_location" {
-  description = "Location of the resource group"
-  value       = azurerm_resource_group.demo.location
+  value = azurerm_resource_group.demo.location
 }
