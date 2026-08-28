4f214dad8c5fe3181c08de085f5427037aa632d5
NiranPrem <163099269+NiranPrem@users.noreply.github.com>
2026-08-28 11:30:21 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.9.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "demo" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = {
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+}
